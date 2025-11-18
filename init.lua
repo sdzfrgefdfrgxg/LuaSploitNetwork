ed9ae11d9af048913cf64f4c14425d0ba653932e
sdzfrgefdfrgxg <LT_RL@outlook.com>
2025-11-18 17:31:10 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,19 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/sdzfrgefdfrgxg/LuaSploitNetwork/refs/heads/main/LSN_Core.lua"))()
+local base = "https://raw.githubusercontent.com/sdzfrgefdfrgxg/LuaSploitNetwork/main/"
+
+local function loadModule(path)
+    return loadstring(game:HttpGet(base .. path))()
+end
+
+local LSN = loadModule("LuaSploitNetworkLib.lua")
+
+-- load modules inside the lib
+LSN.modules = {
+    Keybind = loadModule("modules/keybind.lua"),
+    ColorPicker = loadModule("modules/colorpicker.lua"),
+    Textbox = loadModule("modules/textbox.lua"),
+    Resize = loadModule("modules/resize.lua"),
+    Theme = loadModule("modules/theme.lua"),
+    Notify = loadModule("modules/notifications.lua"),
+}
+
+return LSN
