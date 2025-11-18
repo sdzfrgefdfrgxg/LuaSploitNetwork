81ee55f9724d64601658c484b9fa049bde348f8c
sdzfrgefdfrgxg <LT_RL@outlook.com>
2025-11-18 17:05:18 +0000

    Create LSN_Core.lua with module imports

---
+++ LSN_Core.lua
@@ -0,0 +1,14 @@
+local LSN = {}
+
+LSN.Keybinds      = require(script.modules.Keybinds)
+LSN.Textbox       = require(script.modules.Textbox)
+LSN.ColorPicker   = require(script.modules.ColorPicker)
+LSN.ScriptHub     = require(script.modules.ScriptHub)
+LSN.Resize        = require(script.modules.Resize)
+LSN.WindowControl = require(script.modules.WindowControls)
+LSN.Themes        = require(script.modules.Themes)
+LSN.BlurFX        = require(script.modules.BlurFX)
+LSN.Mobile        = require(script.modules.MobileScaler)
+LSN.Fonts         = require(script.modules.FontLoader)
+
+return LSN
